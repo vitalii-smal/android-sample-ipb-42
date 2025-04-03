53199ff9308a933df71384d396c78ad16fc4f639
Vitalii Smal <vitalii.smal@gmail.com>
2025-04-03 12:11:25 +0300

    Added ViewModel sample

--- app/src/main/java/com/ipb42/android/MainActivity.kt
+++ app/src/main/java/com/ipb42/android/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.ipb42.android
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.ipb42.android.counter.CounterView
+import com.ipb42.android.counter.CounterViewModel
 import com.ipb42.android.ui.theme.AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Students",
+                    CounterView(
+                        viewModel = CounterViewModel(),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,28 +31,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Button(onClick = {
-            val intent = Intent(context, SecondActivity::class.java)
-            context.startActivity(intent)
-        },) {
-            Text(text = "second activity")
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidTheme {
-        Greeting("Android")
+        CounterView(viewModel = CounterViewModel())
     }
 }
